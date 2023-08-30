0946d79baef2d21408102c3c55c85f0d601e5894
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-08-30 20:57:05 +0700

    feat(core): setup category domain, ports, and services

---
+++ internal/core/domain/category.go
@@ -0,0 +1,11 @@
+package domain
+
+import "time"
+
+// Category is an entity that represents a category of product
+type Category struct {
+	ID        uint64    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
